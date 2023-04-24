29573f2b595aac02dc35ee28459c4e68c96db004
juanfarias <juanfariashost@gmail.com>
2023-04-24 14:32:33 -0300

    envio com img e ajustes na sec one

--- src/components/SectionOne.tsx
+++ src/components/SectionOne.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import img_Suco from "../assets/istockphoto-1279829945-612x612-transformed.jpeg"
 
 export const SectionOne:React.FC = () =>{
     return(
         <section className="primeira-secao" id="infosec">
-            <Container>
-            <Row className="align-items-center">
-                    <Col md={6}>
-                        <h2>Informações I</h2>
+            <Container fluid>
+                <Row>
+                    <Col md={4}>
+                    <img src={img_Suco} alt="Imagem" className="img_suquinho" />
                     </Col>
-                    <Col md={6}>
-                        <h2> Informações II</h2>
+                    <Col md={4}>
+                    <h2>Informações II</h2>
+                    <p>Aqui vai o conteúdo do texto de Informações II.</p>
+                    </Col>
+                    <Col md={4}>
+                        <h2>asdasda</h2>
                     </Col>
                 </Row>
             </Container>
